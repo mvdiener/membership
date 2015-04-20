8d04310be1711b312f31fa1a937b864c20fe34a4
Max Diener <mvdiener@gmail.com>
2015-04-20 11:32:53 -0700

    Added helper for session/cookie creation

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,13 @@
+module SessionsHelper
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    if session[:current_user_id]
+      @current_user ||= User.find(session[:current_user_id])
+    end
+  end
+
+end
