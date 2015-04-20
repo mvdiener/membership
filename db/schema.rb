bdf5d6c634d2e4cb3f66dae78157d050726f60a9
Max Diener <mvdiener@gmail.com>
2015-04-20 11:35:31 -0700

    Altered required gems

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20150418181018) do
 
   create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
