4e98000db05fdb92c0af739092d2d9d1829d4821
Max Diener <mvdiener@gmail.com>
2015-04-20 11:32:27 -0700

    Added controller to handle user creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    if current_user
+      redirect_to(root_path)
+    else
+      @user = User.new
+      render 'new'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to root_path
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+    private
+
+     def user_params
+      params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+    end
+
+end
