86f844f2bd8682873c711f6dc111aeeee9d687ee
Max Diener <mvdiener@gmail.com>
2015-04-20 11:32:02 -0700

    Added controller to handle sessions/cookies

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    if current_user
+      redirect_to(root_path)
+    else
+      render 'new'
+    end
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+end
