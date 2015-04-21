50f18b2e0ac8afe6dc4157a0c41be60a656b5c38
Max Diener <mvdiener@gmail.com>
2015-04-20 19:50:29 -0700

    Added associations for users and memberships

---
+++ app/models/club.rb
@@ -0,0 +1,5 @@
+class Club < ActiveRecord::Base
+
+  belongs_to :user
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :clubs
+
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
