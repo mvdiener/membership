7205a1a3e67247a787d8861d0841e3e2bd05b034
Max Diener <mvdiener@gmail.com>
2015-04-20 20:27:22 -0700

    Added new and create methods for memberships

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,9 +1,28 @@
 class ClubsController < ApplicationController
 
   def new
+    @club = Club.new
+  end
+
+  def create
+    @club = Club.new(club_params)
+    @club.user = current_user
+    if @club.save
+      redirect_to(user_path(current_user), method: "GET")
+    else
+      @errors = @club.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
   end
+
+  private
+
+    def club_params
+      params.require(:club).permit(:name, :type, :cost)
+    end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
     private
 
-     def user_params
+    def user_params
       params.require(:user).permit(:username, :email, :password,
                                    :password_confirmation)
     end
