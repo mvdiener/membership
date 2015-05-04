8ab8d18f9f91140d780d7fae1644f2add6a41453
Max Diener <mvdiener@gmail.com>
2015-05-04 15:25:25 -0700

    Added env variable for keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Tell the app that the secret key is an evironment variable
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
 end
